c1ebd55990b7c0f40808d34f608c4a6a4e44e179
cesar-secundary <cesariesperemaria@gmail.com>
2025-01-24 16:16:05 +0100

    modificando db

--- sql/qyueys.sql
+++ sql/qyueys.sql
@@ -12,4 +12,18 @@ SELECT * FROM empleados WHERE poblation = "Madrid";
 
 -- Mostrar todas las empresas junto con el número de empleados asignados a cada una
 
-SELECT *
+SELECT * FROM company.name (SELECT empleados.name(count*));
+SELECT com.name, (SELECT COUNT(*) FROM empleados e WHERE e.cif = com.cif) FROM company com;
+
+-- Actualizar el número de teléfono de un empleado específico usando su dni como identificador
+    UPDATE empleados  SET phone = NULL  WHERE  dni = IS NOT NULL;
+ 
+ -- Eliminar una empresa de la base de datos asegurando que no queden registros huérfanos.
+
+    DELETE FROM company WHERE cif = 12312; -- En el sql usaremos la restricion ON DELETE CASCADE
+
+
+
+
+
+
